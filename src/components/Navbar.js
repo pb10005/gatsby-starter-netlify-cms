19db1ec1dd9e034e9768858e1268c170f7621140
root <atihsa.mihs.ne.ho.da@gmail.com>
2022-05-01 09:25:58 +0900

    Feature: remove unneccessray parts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,16 +67,13 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
-              </Link>
+                Member
+              </Link> 
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
