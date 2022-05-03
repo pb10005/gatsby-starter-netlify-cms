73cd6327e13782aee0abd3919fa28e6645c55293
pb10005 <atihisa.mihs.ne.ho.da@gmail.com>
2022-05-03 09:00:11 +0000

    Feature: rename product page to member page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -45,7 +45,7 @@ export const IndexPageTemplate = ({
                   </div>
                   <div className="columns">
                     <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
+                      <Link className="btn" to="/members">
                         Members
                       </Link>
                     </div>
