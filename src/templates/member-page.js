7e1e777e7c8f3df0b7f9b53cce12a8a1d17aa4c8
pb10005 <atihisa.mihs.ne.ho.da@gmail.com>
2022-05-03 09:06:25 +0000

    Rebuild

---
+++ src/templates/member-page.js
@@ -0,0 +1,109 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import FullWidthImage from "../components/FullWidthImage";
+
+// eslint-disable-next-line
+export const ProductPageTemplate = ({
+  image,
+  title,
+  heading,
+  description,
+  intro,
+  main,
+}) => {
+  const heroImage = getImage(image) || image;
+
+  return (
+    <div className="content">
+      <FullWidthImage img={heroImage} title={title} />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-7 is-offset-1">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  {heading}
+                </h3>
+                <p>{description}</p>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <Features gridItems={intro.blurbs} /> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+ProductPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+};
+
+const ProductPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <ProductPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+      />
+    </Layout>
+  );
+};
+
+ProductPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
+
+export default ProductPage;
+
+export const productPageQuery = graphql`
+  query ProductPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+          }
+        }
+        heading
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+  }
+`;
